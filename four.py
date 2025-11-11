d79fe6c7df1d2c6d530ec17eac17802b171930a3
Ty Chan <chan.ty8191@gmail.com>
2025-11-11 11:55:40 -0500

    Update four.py

--- four.py
+++ four.py
@@ -1,16 +1,36 @@
-import numpy as py
+import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
-import yfinance as yf
+import numpy as np
 import datetime as dt
 
-start = dt.datetime(2010, 1, 1)
-end = dt.datetime.now()
-
-rets = yf.download(tickers = ['SPY', 'MSFT'], start = start, end = end, interval = '1mo', auto_adjust=False)['Adj Close']
-print(rets.head())
+ticker = "SPY"
+end_date = dt.datetime.now()
+start_date = end_date - dt.timedelta(days = 365)
 
 
-rets.plot()
+data = yf.download(ticker, start = start_date, end = end_date, auto_adjust=False)
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize = (12,8), gridspec_kw= {'height_ratios': [4,1]})
+
+ax1.plot(data.index, data['Adj Close'], label = 'Adjusted Close Price', linewidth = 1)
+ma_50 = data['Adj Close'].rolling(window=50, min_periods=0).mean()
+ma_20 = data['Adj Close'].rolling(window=20, min_periods=0).mean()
+ax1.plot(data.index, ma_50, label = "50 Day MA", color = 'orange', linewidth = 0.5)
+ax1.plot(data.index, ma_20, label = "20 Day MA", color = 'red', linewidth = 0.5)
+
+ax1.set_ylabel('Price', fontsize = 12)
+ax1.set_title('SPY Ajusted Close', fontsize = 12, fontweight = 'bold')
+
+color = ['green' if data['Close'].values[i] >= data['Open'].values[i] else 'red' for i in range(len(data))]
+
+ax2.bar(data.index,data['Volume', 'SPY'], color = color)
+ax2.set_xlabel('Date', fontsize = 12)
+
+plt.grid(True, alpha = 0.3)
+
+plt.tight_layout()
+
+
 
 plt.show()
