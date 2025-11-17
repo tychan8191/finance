c8912e32d23591247665141c3eb3ca33a8d6f45b
tychan8191 <chan.ty8191@gmail.com>
2025-11-17 18:49:01 -0500

    tried cumulative return function

--- simpleMeanReversion.py
+++ simpleMeanReversion.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime as dt
-import mplfinance as mpf
 
 def get_stock_data(ticker, startTime, endTime):
     return yf.download(ticker, start = startTime, end = endTime, auto_adjust=False)
@@ -47,9 +46,32 @@ df['cumulative returns'] = (1 + df['mean rev returns']).cumprod()
 df['cumulative returns'].plot()
 plt.title("Cumulative Return")
 
+def cumulative_return(position, prices):
+    common_index = position.index.intersection(prices.index)
+    pos = position.loc[common_index]
+    pri = prices.loc[common_index]
+
+    dollar_change = pri.diff()
+    sum_change = (dollar_change * pos.shift(1)).sum(axis=1)
+    portfolio_value = 1.0 + sum_change.cumsum()
+    
+    # Calculate percentage change from initial value
+    pct_change = ((portfolio_value / 1.0) - 1.0) * 100
+    
+    # Create results DataFrame
+    results = pd.DataFrame({
+        'portfolio_value': portfolio_value,
+        'pct_change': pct_change
+    })
+    
+    return results
+alt = cumulative_return(df['signal'], df['Adj Close'])
+figure5 = plt.figure(figsize = figs)
+alt.plot()
+
 #Comment these out to show certain figures
 #plt.close(figure1)
 plt.close(figure2)
 plt.close(figure3)
-plt.close(figure4)
+#plt.close(figure4)
 plt.show()
